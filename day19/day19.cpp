91d071d01e72684599a5cc3f40f8a4968d3d94c8
aajjbb <jefersonlsiq@gmail.com>
2017-12-22 17:56:44 -0200

    partial solution to day19_1

---
+++ day19/day19.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  string input;
+  vector<string> grid;
+
+  while (getline(cin, input)) {
+    grid.push_back(input);
+  }
+
+  int N = grid.size();
+  int M = grid[0].size();
+
+  vector<int> dx = {-1, 1, 0, 0};
+  vector<int> dy = {0, 0, -1, 1};
+
+  
+  return 0;
+}
